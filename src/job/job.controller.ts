6d45daab527b4cef909a56c4006bb884ddd3fea1
Juan Claudio Caires Pereira <juanccaires@gmail.com>
2024-08-20 20:30:56 -0300

    feat: apply to jobs

--- src/job/job.controller.ts
+++ src/job/job.controller.ts
@@ -8,11 +8,15 @@ import {
   Delete,
   UseInterceptors,
   ParseUUIDPipe,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { JobService } from './job.service';
 import { jobDto } from './dto';
 import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
 import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('job')
 export class JobController {
@@ -31,11 +35,12 @@ export class JobController {
   }
 
   @Post('')
-  @Roles('Company', 'Recruiter')
+  @Roles('Company', 'Recruiter', 'Admin')
   createJob(@Body() dto: jobDto) {
     return this.jobService.createJob(dto);
   }
 
+  @Roles('Company', 'Recruiter', 'Admin')
   @Delete(':id')
   deleteJob(@Param('id', ParseUUIDPipe) id: string) {
     return this.jobService.deleteJob(id);
@@ -50,18 +55,27 @@ export class JobController {
     return this.jobService.sendPortifolio(id, userdto);
   }
   */
+
+  @Roles('User', 'Admin')
+  @Post('apply/:id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  applyToJob(@Param('id') id: string, @Req() req: any) {
+    return this.jobService.applyToJob(id, req.user);
+  }
+
+  @Roles('Company', 'Recruiter', 'Admin', 'User')
   @Get('/company/:id')
-  getCompany(@Param('id', ParseUUIDPipe) id: string) {
+  getCompany(@Param('id') id: string) {
     return this.jobService.getCompany(id);
   }
 
   @Get('/candidates/:id')
-  getCandidates(@Param('id', ParseUUIDPipe) id: string) {
+  getCandidates(@Param('id') id: string) {
     return this.jobService.getCandidates(id);
   }
 
   @Put(':id')
-  update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: jobDto) {
+  update(@Param('id') id: string, @Body() dto: jobDto) {
     return this.jobService.update(id, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto, UpdateUserDto } from './dto';
 import { hash } from 'bcrypt';
+import { CompanyDto } from 'src/company/dto';
 
 @Injectable()
 export class UserService {
